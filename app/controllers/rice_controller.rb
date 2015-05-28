84c958cfc7fb79d418911752b96f4f17ea16355d
IshinFUKUOKA <yoghurt1131@gmail.com>
2015-05-28 17:48:32 +0900

    米の登録機能追加
    米新規登録
    米登録内容編集

--- app/controllers/rice_controller.rb
+++ app/controllers/rice_controller.rb
@@ -5,8 +5,25 @@ class RiceController < ApplicationController
     @users = User.all
   end
 
+  def edit
+    @rice = Rice.find(params[:id])
+    @users = User.all
+  end
+
   def create
+    @rice = Rice.new(rice_params)
+
+    @rice.save
 
     redirect_to rice_index_path
+  end
+
+  def update
+    @rice = Rice.find(params[:id])
+    @rice.update(rice_params)
+    redirect_to rice_index_path
+  end
+  def rice_params
+    params.require(:rice).permit(:name, :price, :buyer_id, :purchased_at)
   end
 end
